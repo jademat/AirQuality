6e7b202b034bec62f7f4832025d2ed51e23b9900
jademat <01088049128a@gmail.com>
2025-02-17 16:23:27 +0900

    Initial commit

---
+++ src/main/java/com/example/jademat/airquality/AirQualityApplication.java
@@ -0,0 +1,15 @@
+package com.example.jademat.airquality;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirQualityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AirQualityApplication.class, args);
+    }
+
+
+
+}
---
+++ src/test/java/com/example/jademat/airquality/AirQualityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jademat.airquality;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirQualityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
